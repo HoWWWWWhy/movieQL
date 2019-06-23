ae2a3478550ba91484b4a9b2a1166545c0acc194
BYUNSEHEE <howwwwwhy@gmail.com>
2019-06-23 11:50:06 +0900

    Mutation

---
+++ graphql/db.js
@@ -0,0 +1,50 @@
+export let movies = [
+    {
+        id: 1,
+        name: "Star Wars",
+        score: 5
+    },
+    {
+        id: 2,
+        name: "Dead Poets Society",
+        score: 100
+    },
+    {
+        id: 3,
+        name: "Little Forest",
+        score: 80
+    },
+    {
+        id: 4,
+        name: "About Time",
+        score: 85
+    }
+];
+
+export const getMovies = () => movies;
+
+export const getById = id => {
+    const filteredMovies = movies.filter(movie => movie.id === id);
+    return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+}
